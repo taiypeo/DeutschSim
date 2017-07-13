e1808a1669b607d07a0eb556a5d4352f3954337a
QwertygidQ <Qwertygid@gmail.com>
2017-07-13 19:19:44 +0300

    Started working on the GUI, IT'S NOT FUNCTIONAL AT ALL YET! Just saving my work until I screw everything up

--- com/qwertygid/deutschsim/DeutschSim.java
+++ com/qwertygid/deutschsim/DeutschSim.java
@@ -9,55 +9,12 @@ import org.apache.commons.math3.linear.FieldVector;
 
 import com.qwertygid.deutschsim.IO.*;
 import com.qwertygid.deutschsim.Logic.*;
+import com.qwertygid.deutschsim.GUI.*;
 
 public class DeutschSim {
 
 	public static void main(String[] args) {
-		String qubits = "01";
-		Table<Gate> gates = new Table<Gate>();
-		gates.add_row();
-		gates.add_row();
-		gates.add_col();
-		gates.add_col();
-		
-		Complex[][] data1 = new Complex[][] {
-				{new Complex(Tools.CONTROL_VALUE), new Complex(0)},
-				{new Complex(0), new Complex(1)}
-		};
-		FieldMatrix<Complex> c = new Array2DRowFieldMatrix<Complex>(data1);
-		Gate control = new Gate("control", c);
-		
-		Complex[][] data2 = new Complex[][] {
-				{new Complex(0), new Complex(1)},
-				{new Complex(1), new Complex(0)}
-		};
-		FieldMatrix<Complex> x = new Array2DRowFieldMatrix<Complex>(data2);
-		Gate pauli_x = new Gate("X", x);
-		
-		Complex[][] data3 = new Complex[][] {
-				{new Complex(1), new Complex(1)},
-				{new Complex(1), new Complex(-1)}
-		};
-		FieldMatrix<Complex> had = new Array2DRowFieldMatrix<Complex>(data3);
-		Complex coefficient = new Complex(1/Math.sqrt(2));
-		had = had.scalarMultiply(coefficient);
-		Gate hadamard = new Gate("hadamard", had);
-		
-		gates.insert_element(hadamard, 0, 0);
-		gates.insert_element(control, 0, 1);
-		gates.insert_element(pauli_x, 1, 1);
-		
-		Circuit sim = new Circuit(gates);
-		FieldVector<Complex> state = sim.operate(qubits);
-		System.out.println(state.toString());
-		
-		Serializer s = new Serializer(qubits, sim);
-		try {
-			s.serialize("state.json");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		GUI g = new GUI();
 	}
 
 }
