8ee96ad2c890c5643abb4d85ef447d3aeb163851
QwertygidQ <Qwertygid@gmail.com>
2017-08-09 14:13:17 +0300

    Changed EventQueue for SwingUtilities

--- com/qwertygid/deutschsim/DeutschSim.java
+++ com/qwertygid/deutschsim/DeutschSim.java
@@ -1,12 +1,12 @@
 package com.qwertygid.deutschsim;
 
-import java.awt.EventQueue;
+import javax.swing.SwingUtilities;
 
 import com.qwertygid.deutschsim.GUI.GUI;
 
 public class DeutschSim {
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				new GUI();
